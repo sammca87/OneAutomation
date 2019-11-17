e3d7079662757d77c1e7e6e3f446e5f75145a23e
Periyasamy <sammca87@gmail.com>
2019-11-17 19:30:05 +0530

    more changes on master branch

---
+++ src/test/java/com/qa/testscripts/login/TC_LoginTest_02.java
@@ -0,0 +1,36 @@
+package com.qa.testscripts.login;
+
+import java.util.Hashtable;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.qa.base.Base;
+import com.qa.listeners.Retry;
+import com.qa.pages.Home;
+import com.qa.pages.Login;
+import com.qa.utils.DataReader;
+
+public class TC_LoginTest_02 extends Base{
+	
+	Login login;
+	Home home;
+	
+	@Test(dataProviderClass = DataReader.class, dataProvider = "dp", retryAnalyzer = Retry.class)
+	public void loginTest(Hashtable<String, String> data) {
+		
+		login = new Login(driver);
+		
+		login.typeLoginID(data.get("username"));
+		login.clickOnNxtBtn();
+		
+		login.typePassword(data.get("password"));
+		home = login.clickOnNxtBtn();
+		
+		Assert.assertTrue(home.getTitle().contains("Inbox"));
+		
+		home.logOut();
+
+	}
+
+}
